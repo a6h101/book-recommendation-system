a702ac8c1574d9f9c06e83267b4c8ca128d2f10e
Archit <architd81@gmail.com>
2026-01-17 12:53:32 +0530

    enabled CORS

--- assets/ml-backend/app/main.py
+++ assets/ml-backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 from app.core.startup import load_resources
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,5 +15,12 @@ app = FastAPI(
     title="Book Recommendation API",
     lifespan=lifespan
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # dev only
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
